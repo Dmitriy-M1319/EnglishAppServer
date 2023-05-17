1aba319b1de99fdaa492c04f48444f91a078201d
Dmitriy Mamonov <dimon.5642014@yandex.ru>
2023-05-17 11:14:36 +0300

    fix cors

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI, Body
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from easygoogletranslate import EasyGoogleTranslate
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/api/translate")
 def translate(data = Body()):
@@ -16,5 +26,5 @@ def translate(data = Body()):
     return JSONResponse(content={"word": result})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
 
